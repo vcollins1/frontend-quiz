7516fb6dd1f072d9c17674e49a75d7b88fb3ec6f
Trelle823 <vontrellecollins@gmail.com>
2023-12-14 11:31:15 -0500

    quiz question active states

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,11 @@
 const selections = document.querySelectorAll(".quiz-banner--select");
+const quizHeader = document.querySelector(".quiz-banner--header");
 const question = document.querySelector(".question__current");
-const answerSelections = document.querySelectorAll(".answer")
+const answerSelections = document.querySelectorAll(".answer");
 const curQuesNum = document.querySelector(".current");
 const maxQuesNum = document.querySelector(".end");
-const options = document.querySelectorAll(".answer__text");
+const options = document.querySelectorAll(".answer__text > span");
+const answerBtn = document.querySelector(".answer__btn");
 let curNum = 2;
 let maxNum;
 
@@ -40,21 +42,122 @@ fetch("./data.json").then(response => {
 });
 
 answerSelections.forEach(selection => {
-    selection.addEventListener("click", e => {
-        document.querySelector(".answer__active").classList.remove("answer__active");
+    selection.addEventListener("click", activeAns);
+});
 
-        if (e.target.parentElement.classList.contains("answer"))
-            e.target.parentElement.classList.add("answer__active");
-        else
-            e.target.classList.add("answer__active");
-    })
-})
+answerBtn.addEventListener("click", e => {
+    if (quizHeader.classList.contains("html")) {
+        const correctAns = data[0]["questions"][curNum - 1]["answer"];
+        const userAns = document.querySelector(".answer__active > .answer__text > span");
+        if (userAns === null) {
+            document.querySelector(".error").style.display = "flex";
+        } else {
+            document.querySelector(".error").style.display = "none";
+            answerSelections.forEach(selection => {
+                selection.removeEventListener("click", activeAns);
+            });
+            if (correctAns === userAns.innerText) {
+                document.querySelector(".answer__active > .answer__text > img").src = "./assets/images/icon-correct.svg";
+                document.querySelector(".answer__active").classList.add("correct");
+                answerBtn.innerText = "Next Question";
+            } else {
+                document.querySelector(".answer__active > .answer__text > img").src = "./assets/images/icon-incorrect.svg";
+                document.querySelector(".answer__active").classList.add("incorrect");
+                document.querySelector(".right").src = "./assets/images/icon-correct.svg";
+                answerBtn.innerText = "Next Question";
+            }
+        }
+    }
+
+    if (quizHeader.classList.contains("css")) {
+        const correctAns = data[1]["questions"][curNum - 1]["answer"];
+        const userAns = document.querySelector(".answer__active > .answer__text > span");
+
+        if (userAns === null) {
+            document.querySelector(".error").style.display = "flex";
+        } else {
+            document.querySelector(".error").style.display = "none";
+            answerSelections.forEach(selection => {
+                selection.removeEventListener("click", activeAns);
+            });
+            if (correctAns === userAns.innerText) {
+                document.querySelector(".answer__active > .answer__text > img").src = "./assets/images/icon-correct.svg";
+                document.querySelector(".answer__active").classList.add("correct");
+                answerBtn.innerText = "Next Question";
+            } else {
+                document.querySelector(".answer__active > .answer__text > img").src = "./assets/images/icon-incorrect.svg";
+                document.querySelector(".answer__active").classList.add("incorrect");
+                document.querySelector(".right").src = "./assets/images/icon-correct.svg";
+                answerBtn.innerText = "Next Question";
+            }
+        }
+    }
+
+    if (quizHeader.classList.contains("javascript")) {
+        const correctAns = data[2]["questions"][curNum - 1]["answer"];
+        const userAns = document.querySelector(".answer__active > .answer__text > span");
+
+        if (userAns === null) {
+            document.querySelector(".error").style.display = "flex";
+        } else {
+            document.querySelector(".error").style.display = "none";
+            answerSelections.forEach(selection => {
+                selection.removeEventListener("click", activeAns);
+            });
+            if (correctAns === userAns.innerText) {
+                document.querySelector(".answer__active > .answer__text > img").src = "./assets/images/icon-correct.svg";
+                document.querySelector(".answer__active").classList.add("correct");
+                answerBtn.innerText = "Next Question";
+            } else {
+                document.querySelector(".answer__active > .answer__text > img").src = "./assets/images/icon-incorrect.svg";
+                document.querySelector(".answer__active").classList.add("incorrect");
+                document.querySelector(".right").src = "./assets/images/icon-correct.svg";
+                answerBtn.innerText = "Next Question";
+            }
+        }
+    }
+
+    if (quizHeader.classList.contains("accessibility")) {
+        const correctAns = data[3]["questions"][curNum - 1]["answer"];
+        const userAns = document.querySelector(".answer__active > .answer__text > span");
+
+        if (userAns === null) {
+            document.querySelector(".error").style.display = "flex";
+        } else {
+            document.querySelector(".error").style.display = "none";
+            answerSelections.forEach(selection => {
+                selection.removeEventListener("click", activeAns);
+            });
+            if (correctAns === userAns.innerText) {
+                document.querySelector(".answer__active > .answer__text > img").src = "./assets/images/icon-correct.svg";
+                document.querySelector(".answer__active").classList.add("correct");
+                answerBtn.innerText = "Next Question";
+            } else {
+                document.querySelector(".answer__active > .answer__text > img").src = "./assets/images/icon-incorrect.svg";
+                document.querySelector(".answer__active").classList.add("incorrect");
+                document.querySelector(".right").src = "./assets/images/icon-correct.svg";
+                answerBtn.innerText = "Next Question";
+            }
+        }
+    }
+});
+
+function activeAns(e) {
+    document.querySelector(".answer__active").classList.remove("answer__active");
+
+    if (e.target.parentElement.classList.contains("answer"))
+        e.target.parentElement.classList.add("answer__active");
+    else if (e.target.parentElement.parentElement.classList.contains("answer"))
+        e.target.parentElement.parentElement.classList.add("answer__active");
+    else
+        e.target.classList.add("answer__active");
+}
 
 function loadQuestionPage(quiz) {
     const head = document.querySelector(".quiz-banner--header");
     head.classList.add(quiz["title"].toLowerCase());
     head.querySelector(".quiz-banner__img").src = `${quiz["icon"]}`;
-    head.querySelector(".quiz-banner__text").innerHTML = quiz["title"];
+    head.querySelector(".quiz-banner__text").innerText = quiz["title"];
     document.querySelector(".quiz").style.display = "block";
 
     curQuesNum.innerHTML = curNum;
@@ -63,6 +166,9 @@ function loadQuestionPage(quiz) {
 
     const answers = quiz["questions"][curNum - 1]["options"];
     options.forEach((option, i) => {
-        option.innerHTML = `${answers[i].replaceAll("<", "&lt;").replace(">", "&gt;")}`;
+        option.innerText = `${answers[i]}`;
+
+        if (answers[i] === quiz["questions"][curNum - 1]["answer"])
+            option.nextElementSibling.classList.add("right");
     })
 }
